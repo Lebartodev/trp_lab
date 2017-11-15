803de5f8af63507072372f17aecb3909b2348f05
Vitaliev <Le_barto@mail.ru>
2017-11-15 21:13:54 +0400

    Add error action

---
+++ src/model/data/response/ResponseException.java
@@ -0,0 +1,13 @@
+package model.data.response;
+
+public class ResponseException {
+    private Exception exception;
+
+    public ResponseException(Exception exception) {
+        this.exception = exception;
+    }
+
+    public Exception getException() {
+        return exception;
+    }
+}
