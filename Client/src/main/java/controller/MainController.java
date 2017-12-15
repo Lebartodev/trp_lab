8e95650b86f9e34a04091d3bcab9fa85cfd9ef8e
lebarto <Le_barto@mail.ru>
2017-12-15 22:15:13 +0400

    Изменил sources папку

--- Client/src/main/java/controller/MainController.java
+++ Client/src/main/java/controller/MainController.java
@@ -1,124 +1,125 @@
 package controller;
 
-import base.Model;
-import io.reactivex.disposables.Disposable;
 import base.Controller;
+import base.Model;
 import base.View;
+import io.reactivex.disposables.Disposable;
+import model.data.request.*;
+import util.MarshallerUtil;
 
 public class MainController extends Controller<Model, View> {
-
     private Disposable subscriptionModel;
 
     public void requestCategories() {
-        this.model().send(new RequestShowCategories()).subscribe(actionData->{
+        this.model().send(MarshallerUtil.marshallAction(new RequestShowCategories(), RequestShowCategories.class)).subscribe(actionData -> {
 
-        },throwable -> {
+        }, throwable -> {
             view().onServerNotStartedException();
         });
     }
 
     public void requestCategory(int id) {
-        this.model().send(new RequestShowMovieList(id)).subscribe();
+        this.model().send(MarshallerUtil.marshallAction(new RequestShowMovieList(id), RequestShowMovieList.class)).subscribe();
     }
 
     public void requestMovie(int id) {
-        this.model().send(new RequestShowMovie(id)).subscribe();
+        this.model().send(MarshallerUtil.marshallAction(new RequestShowMovie(id), RequestShowMovie.class)).subscribe();
     }
 
     public void createMovie(String name, int year, String description, int genreId, int budget) {
-        this.model().send(new RequestCreateMovie(name, year, description, genreId, budget)).subscribe();
+        this.model().send(MarshallerUtil.marshallAction(new RequestCreateMovie(name, year, description, genreId, budget), RequestCreateMovie.class)).subscribe();
     }
 
 
     public void createCategory(String categoryName) {
-        this.model().send(new RequestCreateCategory(categoryName)).subscribe();
+        this.model().send(MarshallerUtil.marshallAction(new RequestCreateCategory(categoryName), RequestCreateCategory.class)).subscribe();
     }
 
 
     public void requestCategoryForEdit(int id) {
-        this.model().send(new RequestStartCategoryEdit(id)).subscribe(actiondata -> {
-            if (actiondata instanceof ResponseStartCategoryEdit)
-                view().openCategoryEditor((ResponseStartCategoryEdit) actiondata);
-            else if (actiondata instanceof ResponseException) {
-                view().onError((ResponseException) actiondata);
-            }
-        });
+        //this.model().send(new RequestStartCategoryEdit(id)).subscribe(actiondata -> {
+//            if (actiondata instanceof ResponseStartCategoryEdit)
+//                view().openCategoryEditor((ResponseStartCategoryEdit) actiondata);
+//            else if (actiondata instanceof ResponseException) {
+//                view().onError((ResponseException) actiondata);
+//            }
+       // });
     }
 
     public void requestMovieForCreate() {
-        this.model().send(new RequestStartCreateMovie()).subscribe(actiondata -> {
-            if (actiondata instanceof ResponseStartMovieEdit)
-                view().openMovieEditor((ResponseStartMovieEdit) actiondata);
-            else if (actiondata instanceof ResponseException) {
-                view().onError((ResponseException) actiondata);
-            }
-        });
+//        this.model().send(new RequestStartCreateMovie()).subscribe(actiondata -> {
+//            if (actiondata instanceof ResponseStartMovieEdit)
+//                view().openMovieEditor((ResponseStartMovieEdit) actiondata);
+//            else if (actiondata instanceof ResponseException) {
+//                view().onError((ResponseException) actiondata);
+//            }
+//        });
 
     }
 
     public void editCategory(int id, String name) {
-        this.model().send(new RequestEndCategoryEdit(id, name)).subscribe();
+        //this.model().send(new RequestEndCategoryEdit(id, name)).subscribe();
     }
 
     public void requestMovieForEdit(int id) {
-        this.model().send(new RequestStartMovieEdit(id)).subscribe(actiondata -> {
-            if (actiondata instanceof ResponseStartMovieEdit)
-                view().openMovieEditor((ResponseStartMovieEdit) actiondata);
-            else if (actiondata instanceof ResponseException) {
-                view().onError((ResponseException) actiondata);
-            }
-        });
+//        this.model().send(new RequestStartMovieEdit(id)).subscribe(actiondata -> {
+//            if (actiondata instanceof ResponseStartMovieEdit)
+//                view().openMovieEditor((ResponseStartMovieEdit) actiondata);
+//            else if (actiondata instanceof ResponseException) {
+//                view().onError((ResponseException) actiondata);
+//            }
+//        });
 
     }
 
     public void closeEditCategory(int id) {
-        this.model().send(new RequestEndCategoryEdit(id, null)).subscribe();
+        //this.model().send(new RequestEndCategoryEdit(id, null)).subscribe();
     }
 
     public void closeEditMovie(int id) {
-        this.model().send(new RequestEndMovieEdit(id, null, 0, null, 0, 0)).subscribe();
+       //this.model().send(new RequestEndMovieEdit(id, null, 0, null, 0, 0)).subscribe();
     }
 
 
     public void editMovie(int id, String name, int year, String description, int genreId, int budget) {
-        this.model().send(new RequestEndMovieEdit(id, name, year, description, genreId, budget)).subscribe();
+        //this.model().send(new RequestEndMovieEdit(id, name, year, description, genreId, budget)).subscribe();
     }
 
 
     public void deleteMovie(int id) {
-        this.model().send(new RequestDeleteMovie(id)).subscribe(actionData -> {
-            if (actionData instanceof ResponseException) {
-                view().onError((ResponseException) actionData);
-            }
-        });
+//        this.model().send(new RequestDeleteMovie(id)).subscribe(actionData -> {
+//            if (actionData instanceof ResponseException) {
+//                view().onError((ResponseException) actionData);
+//            }
+//        });
     }
 
     public void deleteCategory(int id) {
-        this.model().send(new RequestDeleteCategory(id)).subscribe(actionData -> {
-            if (actionData instanceof ResponseException) {
-                view().onError((ResponseException) actionData);
-            }
-        });
+//        this.model().send(new RequestDeleteCategory(id)).subscribe(actionData -> {
+//            if (actionData instanceof ResponseException) {
+//                view().onError((ResponseException) actionData);
+//            }
+//        });
     }
 
     protected void subscribeOnModel() {
-        if (model() != null) {
-            subscriptionModel = model().getPublisher().subscribe(actionData -> {
-                if (actionData instanceof ResponseShowCategories) {
-                    view().onShowCategories((ResponseShowCategories) actionData);
-                } else if (actionData instanceof ResponseShowMovieList) {
-                    view().onShowSingleCategory((ResponseShowMovieList) actionData);
-                } else if (actionData instanceof ResponseShowMovie) {
-                    view().onShowMovie((ResponseShowMovie) actionData);
-                }
-
-            }, Throwable::printStackTrace);
-        }
+//        if (model() != null) {
+//            subscriptionModel = model().getPublisher().subscribe(actionData -> {
+//                if (actionData instanceof ResponseShowCategories) {
+//                    view().onShowCategories((ResponseShowCategories) actionData);
+//                } else if (actionData instanceof ResponseShowMovieList) {
+//                    view().onShowSingleCategory((ResponseShowMovieList) actionData);
+//                } else if (actionData instanceof ResponseShowMovie) {
+//                    view().onShowMovie((ResponseShowMovie) actionData);
+//                }
+//
+//            }, Throwable::printStackTrace);
+//        }
     }
 
     @Override
     public void close() {
-        model().send(new RequestExit()).subscribe();
+        //model().send(new RequestExit()).subscribe();
     }
 
 
--- Shared/src/main/java/util/MarshallerUtil.java
+++ Shared/src/main/java/util/MarshallerUtil.java
@@ -1,4 +1,31 @@
 package util;
 
+import org.w3c.dom.Document;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.PropertyException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 public class MarshallerUtil {
+    public static Document marshallAction(Object o, Class<?> c)  {
+        Document document = null;
+        try {
+            JAXBContext context = JAXBContext.newInstance(c);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            document = db.newDocument();
+
+            marshaller.marshal(o, document);
+        } catch (JAXBException | ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        return document;
+    }
 }
