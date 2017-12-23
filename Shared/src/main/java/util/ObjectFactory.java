73d4f4d57fe00354e3f3232ae2d72e1eb0283a9c
lebarto <Le_barto@mail.ru>
2017-12-23 19:31:08 +0400

    Добавил unmarshaller

--- Shared/src/main/java/util/ObjectFactory.java
+++ Shared/src/main/java/util/ObjectFactory.java
@@ -1,7 +1,6 @@
 package util;
 
-import model.data.request.RequestCreateCategory;
-import model.data.request.RequestShowCategories;
+import model.data.request.*;
 import model.data.response.*;
 
 import javax.xml.bind.annotation.XmlRegistry;
@@ -13,31 +12,82 @@ public class ObjectFactory {
     public RequestShowCategories createRequestShowCategories() {
         return new RequestShowCategories();
     }
-    public RequestCreateCategory createRequestCreateCategory(){
+
+    public RequestCreateCategory createRequestCreateCategory() {
         return new RequestCreateCategory();
+    }
+
+    public RequestCreateMovie createRequestCreateMovie() {
+        return new RequestCreateMovie();
+    }
+
+    public RequestDeleteCategory createRequestDeleteCategory() {
+        return new RequestDeleteCategory();
+    }
+
+    public RequestDeleteMovie createRequestDeleteMovie() {
+        return new RequestDeleteMovie();
+    }
+
+    public RequestEndCategoryEdit createRequestEndCategoryEdit() {
+        return new RequestEndCategoryEdit();
+    }
+
+    public RequestEndMovieEdit createRequestEndMovieEdit() {
+        return new RequestEndMovieEdit();
+    }
+
+    public RequestExit createRequestExit() {
+        return new RequestExit();
+    }
+
+    public RequestShowMovie createRequestShowMovie() {
+        return new RequestShowMovie();
+    }
+
+    public RequestShowMovieList createRequestShowMovieList() {
+        return new RequestShowMovieList();
+    }
+
+    public RequestStartCategoryEdit createRequestStartCategoryEdit() {
+        return new RequestStartCategoryEdit();
+    }
+
+    public RequestStartCreateMovie createRequestStartCreateMovie() {
+        return new RequestStartCreateMovie();
+    }
+
+    public RequestStartMovieEdit createRequestStartMovieEdit() {
+        return new RequestStartMovieEdit();
     }
 
 
     //response
-    public ResponseException createResponseException(){
+    public ResponseException createResponseException() {
         return new ResponseException();
     }
-    public ResponseMovieEditedData createResponseMovieEditedData(){
+
+    public ResponseMovieEditedData createResponseMovieEditedData() {
         return new ResponseMovieEditedData();
     }
-    public ResponseShowCategories createResponseShowCategories(){
+
+    public ResponseShowCategories createResponseShowCategories() {
         return new ResponseShowCategories();
     }
-    public ResponseShowMovie createResponseShowMovie(){
+
+    public ResponseShowMovie createResponseShowMovie() {
         return new ResponseShowMovie();
     }
-    public ResponseShowMovieList createResponseShowMovieList(){
+
+    public ResponseShowMovieList createResponseShowMovieList() {
         return new ResponseShowMovieList();
     }
-    public ResponseStartCategoryEdit createResponseStartCategoryEdit(){
+
+    public ResponseStartCategoryEdit createResponseStartCategoryEdit() {
         return new ResponseStartCategoryEdit();
     }
-    public ResponseStartMovieEdit createResponseStartMovieEdit(){
+
+    public ResponseStartMovieEdit createResponseStartMovieEdit() {
         return new ResponseStartMovieEdit();
     }
 }
