e73c4029bba4ac74301fb4c24c5040f2d9c02b9d
pavel <paul.sav21@gmail.com>
2018-01-06 19:42:11 +0400

    HAAAAAAAAAAAAAA

--- Web/src/main/java/util/HibernateUtil.java
+++ Web/src/main/java/util/HibernateUtil.java
@@ -5,18 +5,10 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
-import java.util.Properties;
-
 public class HibernateUtil {
 
     //XML based configuration
     private static SessionFactory sessionFactory;
-
-    //Annotation based configuration
-    private static SessionFactory sessionAnnotationFactory;
-
-    //Property based configuration
-    private static SessionFactory sessionJavaConfigFactory;
 
     private static SessionFactory buildSessionFactory() {
         try {
@@ -40,48 +32,9 @@ public class HibernateUtil {
     }
 
 
-
-    private static SessionFactory buildSessionJavaConfigFactory() {
-        try {
-            Configuration configuration = new Configuration();
-
-            //Create Properties, can be read from property files too
-            Properties props = new Properties();
-            props.put("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
-            props.put("hibernate.connection.url", "jdbc:mysql://localhost/test");
-            props.put("hibernate.connection.username", "root");
-            props.put("hibernate.connection.password", "admin");
-            props.put("hibernate.current_session_context_class", "thread");
-
-            configuration.setProperties(props);
-
-            //we can set mapping file or class with annotation
-            //addClass(Employee1.class) will look for resource
-            // com/journaldev/hibernate/model/Employee1.hbm.xml (not good)
-            configuration.addAnnotatedClass(Task.class);
-
-            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-            System.out.println("Hibernate Java Config serviceRegistry created");
-
-            SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-
-            return sessionFactory;
-        }
-        catch (Throwable ex) {
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
-
     public static SessionFactory getSessionFactory() {
         if(sessionFactory == null) sessionFactory = buildSessionFactory();
         return sessionFactory;
-    }
-
-
-    public static SessionFactory getSessionJavaConfigFactory() {
-        if(sessionJavaConfigFactory == null) sessionJavaConfigFactory = buildSessionJavaConfigFactory();
-        return sessionJavaConfigFactory;
     }
 
 }
