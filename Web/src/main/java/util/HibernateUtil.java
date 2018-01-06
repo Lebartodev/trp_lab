a3616d2b49ac798d06e7a78af7d364bce6b7dbd4
lebarto <lebartodev@gmail.com>
2018-01-06 21:02:43 +0400

    web

--- Web/src/main/java/util/HibernateUtil.java
+++ Web/src/main/java/util/HibernateUtil.java
@@ -1,24 +1,17 @@
 package util;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Persistence;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
 
 public class HibernateUtil {
 
     //XML based configuration
     private static SessionFactory sessionFactory;
-    @PersistenceContext(unitName="org.hibernate.tutorial.jpa")
-    private static EntityManager entityManager;
+    @PersistenceContext(unitName = "org.hibernate.tutorial.jpa")
+    private static EntityManagerFactory entityManager;
 
     private static SessionFactory buildSessionFactory() {
         try {
@@ -45,7 +38,7 @@ public class HibernateUtil {
 
     public static EntityManager getEntityManager() {
         if (entityManager == null) buildSessionFactory();
-        return entityManager;
+        return entityManager.createEntityManager();
     }
 
 }
