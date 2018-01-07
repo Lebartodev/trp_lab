7013ca35fdaf2708aaad4870271af3e79c3c3c76
pavel <paul.sav21@gmail.com>
2018-01-07 19:23:23 +0400

    HAAAAAAAAAAAAAA

--- Web/src/main/java/CategoriesPage.java
+++ Web/src/main/java/CategoriesPage.java
@@ -1,32 +1,40 @@
 import util.CategoryItem;
 
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
-import javax.faces.component.UIComponent;
 import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 @ManagedBean
 @RequestScoped
 public class CategoriesPage {
-    @ManagedProperty("#{categoriesRepository}")
-    private CategoriesRepository categoriesRepository;
     private UIInput input1;
 
-    public CategoriesRepository getCategoriesRepository() {
-        return categoriesRepository;
+    @EJB(lookup = "java:module/storage")
+    private IControllerSQL controllerSQL;
+
+    public Collection<CategoryItem> getCategories(){
+        List<CategoryItem> categoryItems = new ArrayList<>();
+        try {
+            categoryItems = controllerSQL.getCategories();
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return categoryItems;
     }
 
-    public void setCategoriesRepository(CategoriesRepository categoriesRepository) {
-        this.categoriesRepository = categoriesRepository;
-    }
-
-    public void createCategory(Object name){
-        categoriesRepository.createCategory((String) name);
+    public void createCategory(String name){
+        try {
+            controllerSQL.createCategory(CategoryItem.newBuilder().name(name).build());
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         try {
             FacesContext.getCurrentInstance().getExternalContext().redirect("categoriesList.jsf");
         } catch (IOException e) {
@@ -40,5 +48,31 @@ public class CategoriesPage {
 
     public void setInput1(UIInput input1) {
         this.input1 = input1;
+    }
+
+    public void deleteCategory(Object id){
+        try {
+            controllerSQL.deleteCategory((Integer) id);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().redirect("categoriesList.jsf");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void editCategory(int id, String name){
+        try {
+            controllerSQL.updateCategory(CategoryItem.newBuilder().id(id).name(name).build());
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().redirect("categoriesList.jsf");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- Web/src/main/java/MoviePage.java
+++ Web/src/main/java/MoviePage.java
@@ -1,27 +1,30 @@
 import util.MovieItem;
 
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Collection;
 
 @ManagedBean
 @RequestScoped
 public class MoviePage {
-    @ManagedProperty("#{movieRepository}")
-    private MovieRepository movieRepository;
+    @EJB(lookup = "java:module/storage")
+    private IControllerSQL controllerSQL;
 
     private int selectedId;
     private MovieItem selectedMovie;
 
-    public Collection<MovieItem> getMovies(int id) {
-        return movieRepository.getMovies(id);
+    public Collection<MovieItem> getMovies(int id) throws SQLException, ClassNotFoundException {
+        return controllerSQL.getMoviesInCategory(id);
     }
 
     public void loadMovie() {
         try {
-            selectedMovie = movieRepository.getMovie(selectedId);
+            selectedMovie = controllerSQL.getMovie(selectedId);
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
@@ -39,7 +42,42 @@ public class MoviePage {
         return selectedId;
     }
 
-    public void setMovieRepository(MovieRepository movieRepository) {
-        this.movieRepository = movieRepository;
+    public void createMovie(String name, int year, String description, int genreId, int budget){
+        try {
+            controllerSQL.createMovie(MovieItem.newBuilder().name(name).year(year).description(description).genreId(genreId).budget(budget).build());
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().redirect("movieList.jsf");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteMovie(int id){
+        try {
+            controllerSQL.deleteMovie(id);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().redirect("movieList.jsf");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateMovie(int id, String name, int year, String description, int genreId, int budget){
+        try {
+            controllerSQL.updateMovie(MovieItem.newBuilder().id(id).name(name).year(year).description(description).genreId(genreId).budget(budget).build());
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().redirect("movieList.jsf");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
