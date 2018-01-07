f2eb17d47858e2bcfd783fe68b6b08ca2753d506
lebarto <lebartodev@gmail.com>
2018-01-07 17:09:18 +0400

    web

---
+++ Web/src/main/java/CategoriesPage.java
@@ -0,0 +1,22 @@
+import util.CategoryItem;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import java.util.Collection;
+import java.util.List;
+
+@ManagedBean
+@RequestScoped
+public class CategoriesPage {
+    @ManagedProperty("#{categoriesRepository}")
+    private CategoriesRepository categoriesRepository;
+
+    public CategoriesRepository getCategoriesRepository() {
+        return categoriesRepository;
+    }
+
+    public void setCategoriesRepository(CategoriesRepository categoriesRepository) {
+        this.categoriesRepository = categoriesRepository;
+    }
+}
---
+++ Web/src/main/java/CategoriesRepository.java
@@ -0,0 +1,38 @@
+import util.CatList;
+import util.CategoryItem;
+import util.Util;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import java.io.Serializable;
+import java.sql.SQLException;
+import java.util.*;
+
+@ManagedBean
+@ApplicationScoped
+public class CategoriesRepository implements Serializable {
+    private List<CategoryItem> categoryItems = new ArrayList<>();
+    @EJB(lookup = "java:module/storage")
+    private IControllerSQL controllerSQL;
+
+    @PostConstruct
+    protected void init() {
+        try {
+            categoryItems = controllerSQL.getCategories();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<CategoryItem> getCategoryItems() {
+        return categoryItems;
+    }
+
+    public void setCategoryItems(List<CategoryItem> categoryItems) {
+        this.categoryItems = categoryItems;
+    }
+}
