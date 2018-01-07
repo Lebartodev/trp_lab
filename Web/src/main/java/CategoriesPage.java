785e5dde5ece24eb5d988a756c5de6411631c325
lebarto <lebartodev@gmail.com>
2018-01-07 18:14:57 +0400

    web

--- Web/src/main/java/CategoriesPage.java
+++ Web/src/main/java/CategoriesPage.java
@@ -3,6 +3,10 @@ import util.CategoryItem;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 
@@ -11,6 +15,7 @@ import java.util.List;
 public class CategoriesPage {
     @ManagedProperty("#{categoriesRepository}")
     private CategoriesRepository categoriesRepository;
+    private UIInput input1;
 
     public CategoriesRepository getCategoriesRepository() {
         return categoriesRepository;
@@ -20,7 +25,20 @@ public class CategoriesPage {
         this.categoriesRepository = categoriesRepository;
     }
 
-    public void createCategory(String name){
-        categoriesRepository.createCategory(name);
+    public void createCategory(Object name){
+        categoriesRepository.createCategory((String) name);
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().redirect("categoriesList.jsf");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public UIInput getInput1() {
+        return input1;
+    }
+
+    public void setInput1(UIInput input1) {
+        this.input1 = input1;
     }
 }
